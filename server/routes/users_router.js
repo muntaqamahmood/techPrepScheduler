d9a2dc247195855fb5098ed734c2244965fce85f
Andrew Qian <andrewqian2001@gmail.com>
2023-03-03 21:25:42 -0500

    fix: prettier

--- server/routes/users_router.js
+++ server/routes/users_router.js
@@ -2,7 +2,5 @@ import { Router } from "express";
 export const usersRouter = Router();
 
 usersRouter.get("/", (req, res) => {
-    res.send("Hello World!");
-    }
-);
-
+  res.send("Hello World!");
+});
