60099597aecd443f3cf2147ca36d3d84cae769d4
Andrew Qian <andrewqian2001@gmail.com>
2023-03-06 20:02:18 -0500

    fix: prettier

--- client/src/Page/Schedule.jsx
+++ client/src/Page/Schedule.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import DateTimePicker from 'react-datetime-picker'
+import DateTimePicker from "react-datetime-picker";
 import "../Styles/Schedule.css";
-import 'react-datetime-picker/dist/DateTimePicker.css';
-import 'react-calendar/dist/Calendar.css';
-import 'react-clock/dist/Clock.css';
+import "react-datetime-picker/dist/DateTimePicker.css";
+import "react-calendar/dist/Calendar.css";
+import "react-clock/dist/Clock.css";
 
 const Schedule = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -17,8 +17,13 @@ const Schedule = () => {
   return (
     <div className="schedule">
       <h2>Select Your Available Date for an interview</h2>
-      <DateTimePicker onChange={handleDateChange} value={selectedDate} format={"y-MM-dd  h a"} required={true} amPmAriaLabel={"Select AM/PM"
-}/>
+      <DateTimePicker
+        onChange={handleDateChange}
+        value={selectedDate}
+        format={"y-MM-dd  h a"}
+        required={true}
+        amPmAriaLabel={"Select AM/PM"}
+      />
       <div className="buttons">
         <button>Cancel</button>
         <button>Save</button>
