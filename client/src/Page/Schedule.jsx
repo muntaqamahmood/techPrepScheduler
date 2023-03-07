43c925e75473212f3de105511be5370054280f6b
Andrew Qian <andrewqian2001@gmail.com>
2023-03-07 12:47:56 -0500

    add: fields to form

--- client/src/Page/Schedule.jsx
+++ client/src/Page/Schedule.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import DateTimePicker from "react-datetime-picker";
+import axios from "axios";
 import "../Styles/Schedule.css";
 import "react-datetime-picker/dist/DateTimePicker.css";
 import "react-calendar/dist/Calendar.css";
@@ -7,6 +8,8 @@ import "react-clock/dist/Clock.css";
 
 const Schedule = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
   const handleDateChange = (date) => {
     console.log("huh?");
@@ -14,20 +17,57 @@ const Schedule = () => {
     setSelectedDate(date);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const res = axios.post("/api/schedule", {
+      userId: "testUserId",
+      title,
+      description,
+      selectedDate,
+    });
+
+    console.log("Submitting form...");
+    console.log("Selected date:", selectedDate);
+    console.log("Title:", title);
+    console.log("Description:", description);
+    // Reset form values
+    setSelectedDate(new Date());
+    setTitle("");
+    setDescription("");
+  };
+
   return (
     <div className="schedule">
       <h2>Select Your Available Date for an interview</h2>
-      <DateTimePicker
-        onChange={handleDateChange}
-        value={selectedDate}
-        format={"y-MM-dd  h a"}
-        required={true}
-        amPmAriaLabel={"Select AM/PM"}
-      />
-      <div className="buttons">
-        <button>Cancel</button>
-        <button>Save</button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <label htmlFor="description">Description:</label>
+        <textarea
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        ></textarea>
+        <label htmlFor="datetime">Date and Time:</label>
+        <DateTimePicker
+          id="datetime"
+          onChange={handleDateChange}
+          value={selectedDate}
+          format={"y-MM-dd  h a"}
+          required
+          amPmAriaLabel={"Select AM/PM"}
+        />
+        <div className="buttons">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 };
