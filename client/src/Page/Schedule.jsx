e074f8bcb85673d3ee5775612d27bedb553c5ca8
Andrew Qian <andrewqian2001@gmail.com>
2023-03-06 19:56:23 -0500

    add: date/time picker

--- client/src/Page/Schedule.jsx
+++ client/src/Page/Schedule.jsx
@@ -1,52 +1,30 @@
-import React from "react";
-import "../Styles/Login.css";
-import "../Styles/form.css";
-import "../Styles/col.css";
-import logo from "../media/tpslogo.png";
+import React, { useState } from "react";
+import DateTimePicker from 'react-datetime-picker'
+import "../Styles/Schedule.css";
+import 'react-datetime-picker/dist/DateTimePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-clock/dist/Clock.css';
 
-const Login = () => {
+const Schedule = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    console.log("huh?");
+    console.log(date);
+    setSelectedDate(date);
+  };
+
   return (
-    <div className="container">
-      <div className="logo">
-        <img src={logo} alt="App Logo" />
-      </div>
-      <div className="row">
-        <div className="col-auto"></div>
-      </div>
-
-      <div className="row">
-        <div className="col-auto"></div>
-
-        <div className="col-auto">
-          <div className="login">
-            <form className="complex-form" id="login-form">
-              <div className="form-title">Register / Login</div>
-
-              <div className="form-text"> Email: </div>
-              <input
-                type="text"
-                name="username"
-                className="form-element"
-                placeholder="Enter a username"
-              />
-
-              <div className="form-text"> Password: </div>
-              <input
-                type="password"
-                name="password"
-                className="form-element"
-                placeholder="Enter a password"
-              />
-              <button id="reset" name="action" className="btn">
-                Login with Google
-              </button>
-            </form>
-          </div>
-        </div>
-        <div className="col-auto"></div>
+    <div className="schedule">
+      <h2>Select Your Available Date for an interview</h2>
+      <DateTimePicker onChange={handleDateChange} value={selectedDate} format={"y-MM-dd  h a"} required={true} amPmAriaLabel={"Select AM/PM"
+}/>
+      <div className="buttons">
+        <button>Cancel</button>
+        <button>Save</button>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Schedule;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./Page/Home";
+import Login from "./Page/Login";
+import Schedule from "./Page/Schedule";
 import reportWebVitals from "./reportWebVitals";
 import AboutUs from "./Page/AboutUs";
 
@@ -13,6 +15,7 @@ root.render(
     <Routes>
       <Route path="/" exact element={<Home />} />
       <Route path="/aboutus" exact element={<AboutUs />} />
+      <Route path="/schedule" exact element={<Schedule />} />
     </Routes>
   </BrowserRouter>
 );
