f21868f0402782ef7931f2ee6bf5ec76959cfbf5
Andrew Qian <andrewqian2001@gmail.com>
2023-03-12 21:32:35 -0400

    add: code editor

--- client/src/Page/MockInterview.jsx
+++ client/src/Page/MockInterview.jsx
@@ -1,5 +1,4 @@
 import React, { useRef } from "react";
-
 import Editor from "@monaco-editor/react";
 
 const MockInterview = () => {
@@ -18,18 +17,32 @@ const MockInterview = () => {
 
   const options = {
     selectOnLineNumbers: true,
+    automaticLayout: true, // added automatic layout
+    fontFamily: "Fira Code", // added font family
+    fontSize: 16, // added font size
+    minimap: {
+      enabled: true, // added minimap
+    },
+    renderLineHighlight: "all", // added line highlighting
+    scrollBeyondLastLine: false, // added scroll behavior
+    wordWrap: "on", // added word wrapping
   };
 
   return (
-    <Editor
-      height="90vh"
-      defaultLanguage="javascript"
-      defaultValue="// type your code..."
-      options={options}
-      onChange={onChange}
-      editorDidMount={editorDidMount}
-    />
+    <div className="codeEditor">
+      <Editor
+        height="90vh"
+        defaultLanguage="javascript"
+        defaultValue="// type your code..."
+        options={options}
+        onChange={onChange}
+        editorDidMount={editorDidMount}
+        fontFamily={"Fira Code"} // added font family
+        fontSize={16} // added font size
+        value={`//type your code`} // added default value
+      />
+    </div>
   );
-}
+};
 
 export default MockInterview;
