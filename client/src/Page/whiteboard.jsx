fea7ee8ed45639cf334948ce1e3ee624f4cbb65c
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2023-03-16 21:30:03 -0400

    Whiteboard using React Flow (#20)
    * start react flow
    
    * make nodes interactive
    
    * whiteboard WIP (define add node button and custome node)
    
    * whiteboard done (allow creating custom node and connect edge / remove node)
    
    * run prettier
    
    * fix: import
    
    * fix:prettier
    
    ---------
    
    Co-authored-by: Shence <ysc1412799032@163.com>

---
+++ client/src/Page/whiteboard.jsx
@@ -0,0 +1,125 @@
+import { useCallback, useState } from "react";
+import ReactFlow, {
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+  ReactFlowProvider,
+  useReactFlow,
+} from "reactflow";
+import "reactflow/dist/style.css";
+
+import TextUpdaterNode from "./TextUpdaterNode.js";
+
+import "../Styles/text-updater-node.css";
+
+import "../Styles/whiteboard-button.css";
+
+const rfStyle = {
+  backgroundColor: "#B8CEFF",
+};
+
+const edgeOptions = {
+  animated: true,
+  style: {
+    stroke: "white",
+  },
+};
+
+const connectionLineStyle = { stroke: "white" };
+
+let nodeId = 0;
+
+const initialNodes = [
+  {
+    id: "node-1",
+    type: "textUpdater",
+    position: { x: 0, y: 0 },
+    data: { value: 123 },
+  },
+  {
+    id: "node-2",
+    type: "textUpdater",
+    targetPosition: "top",
+    position: { x: 0, y: 200 },
+    data: { label: "node 2" },
+  },
+  {
+    id: "node-3",
+    type: "textUpdater",
+    targetPosition: "top",
+    position: { x: 200, y: 200 },
+    data: { label: "node 3" },
+  },
+];
+
+// we define the nodeTypes outside of the component to prevent re-renderings
+// you could also use useMemo inside the component
+const nodeTypes = { textUpdater: TextUpdaterNode };
+
+const initialEdges = [
+  { id: "edge-1", source: "node-1", target: "node-2", sourceHandle: "a" },
+  { id: "edge-2", source: "node-1", target: "node-3", sourceHandle: "b" },
+];
+
+const Whiteboard = () => {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+  const onConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
+  const reactFlowInstance = useReactFlow();
+  const onClick = useCallback(() => {
+    const id = `${++nodeId}`;
+    const newNode = {
+      id,
+      type: "textUpdater",
+      position: {
+        x: Math.random() * 500,
+        y: Math.random() * 500,
+      },
+      data: {
+        label: `Node ${id}`,
+      },
+    };
+    reactFlowInstance.addNodes(newNode);
+  }, []);
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        defaultEdgeOptions={edgeOptions}
+        connectionLineStyle={connectionLineStyle}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        fitView
+        style={rfStyle}
+      />
+      <button onClick={onClick} className="btn-add">
+        add node
+      </button>
+    </div>
+  );
+};
+
+export default function () {
+  return (
+    <ReactFlowProvider>
+      <Whiteboard />
+    </ReactFlowProvider>
+  );
+}
