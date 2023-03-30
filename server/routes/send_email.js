a8930ced2aab9afc911761798d74fd97c1d63fd5
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2023-03-30 10:58:00 -0400

    feedback:WIP

---
+++ server/routes/send_email.js
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import sgMail from "@sendgrid/mail";
+
+export const emailRouter = Router();
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// @route   POST api/feedback
+// @desc    Send email
+// @access  Private
+emailRouter.post("/", async (req, res) => {
+  try {
+    console.log("Print reqBody", req.body);
+    const userEmail = req.body.userEmail;
+    const feedback = req.body.feedbackData;
+    console.log("Print subject", feedback.subject);
+    console.log("Print feedback", feedback.feedback);
+    console.log(userEmail);
+    const msg = {
+      to: "techprepcheduler@gmail.com", // Change to your recipient
+      from: userEmail, // Change to your verified sender
+      subject: feedback.subject,
+      text: feedback.feedback,
+      html: `<strong>${feedback.feedback}</strong>`,
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log("Email sent");
+      })
+      .catch((error) => {
+        console.error(error);
+        console.info();
+      });
+  } catch (err) {
+    console.log(err);
+    console.info();
+  }
+});
